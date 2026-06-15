256b79a972cbab3ba415c1d721b52f5e217f5d17
Lucbgr <lucas.silva2221e@gmail.com>
2026-06-15 11:31:48 -0400

    1.18.23

--- src/usuario/usuario.dto.ts
+++ src/usuario/usuario.dto.ts
@@ -36,8 +36,8 @@ export class CreateUsuarioDto {
   @IsOptional() @Type(() => Number) @IsInt()
   vendas_rep_codigo?: number;
 
-  @ApiPropertyOptional({ example: 'VAREJO', enum: ['VAREJO', 'ATACADO', 'SUPERVISAO_ATACADO'], description: 'Hub de vendas inicial' })
-  @IsOptional() @IsIn(['VAREJO', 'ATACADO', 'SUPERVISAO_ATACADO'])
+  @ApiPropertyOptional({ example: 'VAREJO', enum: ['VAREJO', 'ATACADO', 'SUPERVISAO_ATACADO', 'GERENCIA'], description: 'Hub de vendas inicial' })
+  @IsOptional() @IsIn(['VAREJO', 'ATACADO', 'SUPERVISAO_ATACADO', 'GERENCIA'])
   vendas_hub_inicial?: string;
 }
 
